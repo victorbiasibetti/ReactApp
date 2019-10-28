0a475cdef1f918e5148e7a5f2075d33a525c8bc6
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-10-28 20:12:19 -0300

    adição de estilos a pagina inicial

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repostiórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
